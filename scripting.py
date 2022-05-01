416410d0b059c064b70c99edd0b89959e170a485
Teguh Permana 666 <teguhpermana096@gmail.com>
2022-05-02 02:06:50 +0800

    [ Scraping footprinting ]

--- scripting.py
+++ scripting.py
@@ -1,1 +1,16 @@
 # Scripts data from
+from email import header
+from wsgiref import headers
+import requests
+link = requests.get("https://quikr.com/")
+print(link)
+# i have respone 403 => 403 the succes code
+
+#content of requesr
+print(link.content)
+
+# confirm the object link
+print(link.url)
+
+# status the code
+print(link.status_code)

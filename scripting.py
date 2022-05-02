56742dfb2162db09337ca54b0cab783552380c22
Teguh Permana 666 <teguhpermana096@gmail.com>
2022-05-02 16:14:09 +0800

    [ Scrapping Data ]

--- scripting.py
+++ scripting.py
@@ -1,21 +1,35 @@
 # Scripts data from
 import requests
-agent = {"User-Agent":'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
-link = requests.get("https://quikr.com/",headers=agent)
-print(link)
+headers= {
+    'User-Agent':'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
+}
+link = requests.get("https://quikr.com/",headers=headers)
+# print(link)
 # i have respone 403 => 403 the succes code
 
 #content of requesr
-print(link.content)
+# print(link.content)
 
 # confirm the object link
-print(link.url)
+# print(link.url)
 
 # status the code
-print(link.status_code)
+# print(link.status_code)
 
 from bs4 import BeautifulSoup
 from bs4 import BeautifulSoup as bs
 
 soup = BeautifulSoup(link.content,"html.parser")
-print(soup.prettify())
+# print(soup.prettify())
+
+# get the title
+print(soup.title)
+
+# get the name of tag
+print(soup.title.name)
+
+# get the parent tage
+print(soup.title.parent.name)
+
+s = soup.find('div', class_='entry-content')
+content = s.find_all('p')
